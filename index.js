ba2ae0291d09b797201bd85512621716fb787e11
ibtg <ibtg0409@gmail.com>
2020-08-21 12:46:24 +0900

    add mongodb connection

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const config = require('./server/config/key');
 
+// mongoDB connection
+const mongoose = require('mongoose');
+mongoose
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('MomgoDB connected ...'))
+  .catch((err) => console.log(err));
+
+// express connection
+const port = 5000;
 app.get('/', (req, res) => res.send('Hello world'));
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`);
