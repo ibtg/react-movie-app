0b16fe5f7d62616931aa7b36b5da2cec781d34a6
ibtg <ibtg0409@gmail.com>
2020-08-22 16:58:35 +0900

    add login endpoint in backend
    use jwt to make token and use cookie parser to make cookie

--- server/index.js
+++ server/index.js
@@ -2,13 +2,50 @@ const express = require('express');
 const app = express();
 const config = require('./config/key');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const { User } = require('./models/User');
 
 // body parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// reigiter
+// cookie parser
+app.use(cookieParser());
+
+// login
+app.post('/login', (req, res) => {
+  // 1. find user in DB
+  User.findOne({ email: req.body.email }, (err, user) => {
+    //console.log('usre: ', user);
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: 'Can not find user',
+      });
+    }
+    // 2.if email is found in DB, then password check
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      // Incorrec Password
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: 'Incorrect Password',
+        });
+
+      // 3. Correct Password, generate token
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        res
+          .cookie('x_auth', user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
+    });
+  });
+});
+
+// reigister
 app.post('/register', (req, res) => {
   const user = new User(req.body);
 
