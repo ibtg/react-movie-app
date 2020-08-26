ce40bc70d1e27838d7247115ee4c60af3b445d65
ibtg <ibtg0409@gmail.com>
2020-08-26 21:11:47 +0900

    remove unnecessary comments in backend

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,18 +3,18 @@ const { User } = require('../models/User');
 let auth = (req, res, next) => {
   // get token from cookie
   let token = req.cookies.x_auth;
-  console.log('auto token: ', token);
+  //console.log('auto token: ', token);
 
   User.findByToken(token, (err, user) => {
-    console.log('findByToken');
+    // console.log('findByToken');
     if (err) throw err;
     if (!user) return res.json({ isAuth: false, error: true });
 
     req.token = token;
     req.user = user;
 
-    console.log('auto user: ', user);
-    console.log('asdfsdf');
+    // console.log('auto user: ', user);
+
     next();
   });
 };
