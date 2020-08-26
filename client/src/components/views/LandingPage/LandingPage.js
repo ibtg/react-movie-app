0a17e7e04ccae011db484aff1b3c2a899f3d4271
ibtg <ibtg0409@gmail.com>
2020-08-26 20:16:26 +0900

    add logout feature
    add logout feature in landing LandingPage
    logout feature does not implemented using redux

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -2,11 +2,27 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 import { withRouter } from 'react-router-dom';
 
-function LandingPage() {
+function LandingPage(props) {
   useEffect(() => {
     axios.get('/api/hello').then((response) => console.log(response));
   }, []);
-  return <div>LandingPage</div>;
+
+  const onClickHandler = () => {
+    axios.get('/api/users/logout').then((response) => {
+      if (response.data.success) {
+        props.history.push('login');
+      } else {
+        alert('Faild To Sign Out');
+      }
+    });
+  };
+
+  return (
+    <div>
+      LandingPage
+      <button onClick={onClickHandler}>Sign Out</button>
+    </div>
+  );
 }
 
 export default withRouter(LandingPage);
