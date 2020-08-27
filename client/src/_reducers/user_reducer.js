eaf7d971ffa774ade284e6a21c575fe473a5854d
ibtg <ibtg0409@gmail.com>
2020-08-28 02:54:03 +0900

    Add AUTH in reducer

--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -10,6 +10,8 @@ export default function (state = {}, action) {
     case REGISTER_USER:
       return { ...state, register: action.payload };
       break;
+    case AUTH_USER:
+      return { ...state, userData: action.payload };
     default:
       return state;
   }
