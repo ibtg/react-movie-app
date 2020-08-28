5290f9ed049d52d315ce96fd16de412605da272c
ibtg <ibtg0409@gmail.com>
2020-08-28 17:41:19 +0900

    Add Config info for Movies in Config.js

---
+++ client/src/components/Config.js
@@ -0,0 +1,8 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+export const API_KEY = '7ee72738cff3524ddd4939c14b775e65';
+
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
