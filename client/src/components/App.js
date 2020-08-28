901f61bc467d33be47f50c5a04216eadb3383c31
ibtg <ibtg0409@gmail.com>
2020-08-28 17:39:58 +0900

    Add Suspense in App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import LandingPage from './views/LandingPage/LandingPage';
@@ -9,18 +9,20 @@ import Navbar from './views/Navbar/Navbar';
 
 function App() {
   return (
-    <Router>
-      <Navbar></Navbar>
-      <Switch>
-        <Route exact path="/" component={Auth(LandingPage, null)}></Route>
-        <Route exact path="/login" component={Auth(LoginPage, false)}></Route>
-        <Route
-          exact
-          path="/register"
-          component={Auth(RegisterPage, false)}
-        ></Route>
-      </Switch>
-    </Router>
+    <Suspense fallback={<div>Loaidng</div>}>
+      <Router>
+        <Navbar></Navbar>
+        <Switch>
+          <Route exact path="/" component={Auth(LandingPage, null)}></Route>
+          <Route exact path="/login" component={Auth(LoginPage, false)}></Route>
+          <Route
+            exact
+            path="/register"
+            component={Auth(RegisterPage, false)}
+          ></Route>
+        </Switch>
+      </Router>
+    </Suspense>
   );
 }
 
