3ca9ee7b026269220c6101f0d8b92328b83613bb
ibtg <ibtg0409@gmail.com>
2020-09-07 03:07:38 +0900

    Add LikePage styling
    Show movie image and remove button to top right corner

--- client/src/components/views/LikePage/LikePage.js
+++ client/src/components/views/LikePage/LikePage.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
 import { IMAGE_BASE_URL } from '../../Config';
+import { TiDeleteOutline } from 'react-icons/ti';
+
+import './LikePage.scss';
 
 function LikePage() {
   const [likes, setLikes] = useState([]);
@@ -14,11 +17,9 @@ function LikePage() {
       userFrom: localStorage.getItem('userId'),
     }).then((response) => {
       if (response.data.success) {
-        console.log(response.data);
-        console.log(response.data.likes);
         setLikes(response.data.likes);
       } else {
-        alert('영화 정보를 가져오는데 실패했습니다');
+        alert('Failed to get liked Movie');
       }
     });
   };
@@ -33,32 +34,35 @@ function LikePage() {
       if (response.data.success) {
         fetchLikedMovie();
       } else {
-        alert('리스트에서 지우는데 실패했습니다');
+        alert('Failed to remove from the list');
       }
     });
   };
 
   const renderLikes = likes.map((like, index) => {
     return (
-      <li key={index}>
-        <span>{like.movieTitle}</span>
-        <span>{like.movieRunTime}</span>
+      <li key={index} className="like__item">
+        <img
+          className="like__image"
+          src={`${IMAGE_BASE_URL}w300${like.moviePost}`}
+          alt={like.movieTitle}
+        />
         <button
+          className="like__removeBtn"
           onClick={() => {
             onClickDelete(like.movieId, like.userFrom);
           }}
         >
-          remove
+          <TiDeleteOutline></TiDeleteOutline>
         </button>
       </li>
     );
   });
 
   return (
-    <div style={{ width: '85%', margin: '3rem auto' }}>
-      <h2>FavoritePage</h2>
-      <ul>{renderLikes}</ul>
-    </div>
+    <section className="likePage__container">
+      <ul className="like__lists">{renderLikes}</ul>
+    </section>
   );
 }
 
