fd1c6cbe17862870c9a2bb204b35835663702d85
ibtg <ibtg0409@gmail.com>
2020-09-07 17:16:44 +0900

    Remove unnecessary state and element

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -8,12 +8,10 @@ import { MdKeyboardArrowDown } from 'react-icons/md';
 function LandingPage(props) {
   const [Movies, setMovies] = useState([]);
   const [CurrentPage, setCurrentPage] = useState(1);
-  const [visible, setVisible] = useState(true);
 
   useEffect(() => {
     const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&languate=en-US&page=1`;
     fetchMovies(endpoint).then((response) => {
-      console.log(response);
       setMovies([...Movies, ...response.results]);
       setCurrentPage(response.page + 1);
     });
@@ -36,15 +34,13 @@ function LandingPage(props) {
     <div className="container">
       {/* <h2 className="movie__category">Popular Movie Lists</h2> */}
       {Movies.length !== 0 && <Grid movies={Movies}></Grid>}
-      {visible && (
-        <div className="movie__buttonContainer">
-          <button className="movie__button" onClick={loadMoreItems}>
-            <MdKeyboardArrowDown
-              style={{ width: '100%', height: '40px' }}
-            ></MdKeyboardArrowDown>
-          </button>
-        </div>
-      )}
+      <div className="movie__buttonContainer">
+        <button className="movie__button" onClick={loadMoreItems}>
+          <MdKeyboardArrowDown
+            style={{ width: '100%', height: '40px' }}
+          ></MdKeyboardArrowDown>
+        </button>
+      </div>
     </div>
   );
 }
