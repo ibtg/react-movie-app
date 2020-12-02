66072f75aec26d9cee530a225f318736bb09d2c4
ibtg <ibtg0409@gmail.com>
2020-12-02 15:20:22 +0900

    Remove unnecessary files

--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -3,8 +3,8 @@ import { LOGIN_USER, REGISTER_USER, AUTH_USER } from '../_actions/types';
 export default function (state = {}, action) {
   switch (action.type) {
     case LOGIN_USER:
-      //console.log('LOGIN_USER :', action.payload);
-      //console.log('return: ', { ...state, loginSuccess: action.payload });
+      console.log('LOGIN_USER :', action.payload);
+      console.log('return: ', { ...state, loginSuccess: action.payload });
       return { ...state, loginSuccess: action.payload };
       break;
     case REGISTER_USER:
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
 import promiseMiddleWare from 'redux-promise';
@@ -30,7 +29,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
