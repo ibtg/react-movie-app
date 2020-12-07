e0a4f4c4ece4158eae65f8006768e883325f4efa
ibtg <ibtg0409@gmail.com>
2020-12-07 20:34:24 +0900

    Add most trending movie postr on landing page

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,52 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
-import { API_URL, API_KEY } from '../../Config';
+import { API_URL, API_KEY,IMAGE_BASE_URL } from '../../Config';
 import './LandingPage.scss';
 import Display from '../DisplayPage/Display'
 import { MdKeyboardArrowDown } from 'react-icons/md';
+import ContentsInfo from '../contentsPage/ContentsInfo'
 
 function LandingPage(props) {
-  const [trending, setTrending] = useState([])
-  const [popularMovie, setPopularMovie] = useState([]);
-  const [topRatedMovie, setTopRatedMovie] = useState([]);
-  const [nowPlayingMovie, setNowPlayingMovie] = useState([]);
-  const [upcomingMovie, setupcomingMovie] = useState([])
-  
-
+  const [movieObj, setMovieObj] = useState({})
   useEffect(() => {
-    // TRENDING
-    const trendingUrl = `${API_URL}trending/movie/day?api_key=${API_KEY}`
-    fetchPages(trendingUrl).then((response) => {
-      setTrending(response.results)
-    })
 
-    // MOVIE - POPULAR
-    const popularMovieUrl = `${API_URL}movie/popular?api_key=${API_KEY}`
-    fetchPages(popularMovieUrl).then((response) => {
-      setPopularMovie(response.results)
-    })
+      const fetchData = async () => {
+      const trendingUrl = `${API_URL}trending/movie/day?api_key=${API_KEY}`
+      const popularMovieUrl = `${API_URL}movie/popular?api_key=${API_KEY}`
+      const topRatedMovieUrl = `${API_URL}movie/top_rated?api_key=${API_KEY}`
+      const nowPlayingMovieUrl = `${API_URL}movie/now_playing?api_key=${API_KEY}`
+      const upcomingMovieUrl = `${API_URL}movie/upcoming?api_key=${API_KEY}`
 
-    // MOVIE - TOP RATED
-    const topRatedMovieUrl = `${API_URL}movie/top_rated?api_key=${API_KEY}`
-    fetchPages(topRatedMovieUrl).then((response) => {
-      setTopRatedMovie(response.results)
-    })
-
-    // MOVIE - NOW PLAYING
-    const nowPlayingMovieUrl = `${API_URL}movie/now_playing?api_key=${API_KEY}`
-    fetchPages(nowPlayingMovieUrl).then((response) => {
-      setNowPlayingMovie(response.results)
-    })
-
-    // MOVIE - UP COMING
-    const upcomingMovieVUrl = `${API_URL}movie/upcoming?api_key=${API_KEY}`
-    fetchPages(upcomingMovieVUrl).then((response) => {
-      setupcomingMovie(response.results)
-    })
-
-
+      await Promise.all([
+        fetchPages(trendingUrl),
+        fetchPages(popularMovieUrl),
+        fetchPages(topRatedMovieUrl),
+        fetchPages(nowPlayingMovieUrl),
+        fetchPages(upcomingMovieUrl)])
+        .then(([trending, popular, top, nowpaying, upcoming])=>{
+          setMovieObj({
+          'TRENDING': trending.results, 
+          'POPULAR MOVIES': popular.results,
+          'TOP RATED MOVIES':top.results,
+          "NOW PLAYING MOVIES":nowpaying.results,
+          "UP COMING MOVIES":upcoming.results
+        })})
+    }
+    fetchData()
 
   }, []);
+
+  console.log("trending: ", movieObj.TRENDING&& movieObj.TRENDING[0])
+  // console.log("trending: ", Object.keys(movieObj))
+  // console.log("trending: ", trending)
 
   // const loadMoreItems = () => {
   //   const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&languate=en-US&page=${CurrentPage}`;
@@ -62,34 +54,38 @@ function LandingPage(props) {
   };
 
   return (
-    <div className="container">
-      <Display 
-      pages={trending} 
-      title="TRENDING"
-      ></Display>
+    <>
+      {movieObj.TRENDING && 
+      <div className="landingPage__imageContainer">
+        <div className="landingPage__info">
+          <h1 className="landingPage__trending">TRENDING MOVIE</h1>
+          <h2 className="landingPage__title">{movieObj.TRENDING[0].title}</h2>
+          <p className="landingPage__overview">{movieObj.TRENDING[0].overview}</p>
+        </div>
 
-      <Display 
-      pages={popularMovie} 
-      title="POPULAR MOVIES"
-      ></Display>
+        <img
+        className="landingPage__image"
+        src={`${IMAGE_BASE_URL}w1280${movieObj.TRENDING[0].backdrop_path}` }
+        />
+        </div>
+      }
+      <div className="container">
+      {
+      Object.keys(movieObj).length !==0 && 
+      Object.keys(movieObj).map((category, index) =>(
+        <Display
+        key={index}
+        pages={movieObj[category]}
+        title={category}
+        ></Display>
+        ))
+        }
+      </div>
+    </>
 
-      <Display 
-      pages={topRatedMovie} 
-      title="TOP RATED MOVIES"
-      ></Display>
-      
-      <Display 
-      pages={nowPlayingMovie} 
-      title="NOW PLAYING MOVIES"
-      ></Display>
-      
-      <Display 
-      pages={upcomingMovie} 
-      title="UP COMING MOVIES"
-      ></Display>
-      
-    </div>
-  );
+ 
+
+    );
 }
 
 export default withRouter(LandingPage);
